0e0d9aad5b16eb04b01bd63df4f5a0fdc154bd6e
Jeffry Gonzalez <jeff@hypertheory.com>
2024-08-12 15:56:11 -0400

    done with basic types

--- syntax/src/stuff/02.stuff2.ts
+++ syntax/src/stuff/02.stuff2.ts
@@ -48,3 +48,39 @@ export function sumThemWithUnions(a: SummableItems, b: SummableItems) {
         return [...a, ...b].reduce((l,r) => l +r)
     }
 }
+
+
+export const calculateTaxForFullTime = (n:number) => n * .2;
+export const calculateTaxForContractor = (n: number) => n * 0;
+type TaxablePayCalculation = (n:number) => number;
+export function calculateWeeklyPay(
+    annualPay: number, 
+    taxableFn: TaxablePayCalculation )
+    {
+   const pay = annualPay / 12;
+    return {
+        weeklyPay: Math.round(annualPay / 12),
+        taxableAmount: Math.round(taxableFn(pay))
+    }
+}
+
+export function tagMaker(tag:string) {
+    return (content:string) => `<${tag}>${content}</${tag}>`;
+}
+
+export function tagMakerOldSkool(tag:string, content: string) {
+    return `<${tag}>${content}</${tag}>`
+}
+
+export class TagMaker {
+
+    #tag:string;
+   
+    constructor(tag:string) {
+        this.#tag = tag;
+    }
+
+    make(content:string) {
+        return  `<${this.#tag}>${content}</${this.#tag}>`
+    }
+}
