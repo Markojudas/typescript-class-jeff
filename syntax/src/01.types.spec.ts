d5699908e062af639aedde09e6c1508e13b79a8f
Jeffry Gonzalez <jeff@hypertheory.com>
2024-08-12 14:50:49 -0400

    functions

--- syntax/src/01.types.spec.ts
+++ syntax/src/01.types.spec.ts
@@ -1,46 +1,69 @@
-import { describe, test, expect} from 'vitest'
+import { describe, test, expect } from "vitest";
 describe("Opting in to Type Checking", () => {
-    test('Uninitialized is Any', () => {
-        let age;
-        age = 13;
-        
+  test("Uninitialized is Any", () => {
+    let age;
+    age = 13;
 
-        // 3 hours later
+    // 3 hours later
 
-        age = 'Old';
+    age = "Old";
+  });
 
+  test("Initialize and the type is inferred", () => {
+    let age = 13;
 
+    age = 14;
+    expect(typeof age).toBe("number");
 
-    })
+    //  @ts-expect-error
+    age = "Old";
 
-    test('Initialize and the type is inferred', () => {
-        let age = 13;
+    expect(typeof age).toBe("string");
+  });
 
-        age = 14;
-        expect(typeof age).toBe("number");
+  test("unitialized should have an annotation", () => {
+    let age: number;
+    age = 13;
 
-        //  @ts-expect-error
-        age = 'Old';
+    // 3 hours later
+    // @ts-expect-error
+    age = "Old";
+  });
+  test("unitialized should have an annotation", () => {
+    let age: number | string = 13;
 
-        expect(typeof age).toBe("string")
+    age = "Old";
+  });
 
-    })
+  test("why var is bad", () => {
+    if (true) {
+      var x = 12;
+    }
 
-    test("unitialized should have an annotation", () => {
-        let age:number;
-        age = 13;
-        
+    expect(x).toBe(12); // leaks
+  });
 
-        // 3 hours later
-        // @ts-expect-error 
-        age = 'Old';
+  test("equality", () => {
+    let age = 16;
 
-    })
-    test("unitialized should have an annotation", () => {
-        let age:number | string = 13;
-        
-        age = 'Old';
+    let areEqual = age === +"16";
+    let notEqual = age !== +"17";
+    expect(areEqual).toBe(true);
+    expect(notEqual).toBe(true);
+  });
 
-    })
-})
+  test("truthiness", () => {
+    // if a non boolean value is converted to a boolean, does it convert to  true or false.
 
+    let isTruthy = !!"dog";
+    expect(isTruthy).toBe(true);
+    expect("dog").toBeTruthy();
+    expect(" ").toBeTruthy();
+    expect("").toBeFalsy();
+    expect(0).toBeFalsy();
+    expect(-1).toBeTruthy(); // but any number aside from 0 is truthy
+    expect({}).toBeTruthy();
+    expect(null).toBeFalsy();
+    expect(undefined).toBeFalsy();
+  });
+});
