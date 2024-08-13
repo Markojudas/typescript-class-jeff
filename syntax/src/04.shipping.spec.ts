ca16e5648b939aea89f41da1ab689f943531df45
Jeffry Gonzalez <jeff@hypertheory.com>
2024-08-13 12:27:51 -0400

    before lunch

--- syntax/src/04.shipping.spec.ts
+++ syntax/src/04.shipping.spec.ts
@@ -1,13 +1,16 @@
 import { describe, test } from "vitest";
-import { calculateExpectedDeliveryDate, Order } from "./shipping";
+import {
+  calculateExpectedDeliveryDate,
+  isUndeliveredOrder,
+  Order,
+} from "./shipping";
 
 describe("Shipping Orders", () => {
   test("shipped-orders", () => {
     const order: Order = {
-      status: "shipped",
+      status: "pending",
       customerId: "99",
       orderId: "12",
-      shipDate: "2024-08-13 ",
     };
 
     const expectedDate = calculateExpectedDeliveryDate(order);
@@ -40,5 +43,36 @@ describe("Shipping Orders", () => {
     const expectedDate = calculateExpectedDeliveryDate(order);
 
     // some kind of expectation
+  });
+
+  test("more real world", () => {
+    const order1: Order = {
+      status: "shipped",
+      customerId: "99",
+      orderId: "12",
+      shipDate: "2024-08-13 ",
+    };
+
+    const order2: Order = {
+      status: "pending",
+      customerId: "99",
+      orderId: "12",
+    };
+
+    const order3: Order = {
+      status: "delivered",
+      customerId: "99",
+      orderId: "12",
+      shipDate: "2024-08-13 ",
+      deliveryDate: "2024-08-14",
+    };
+
+    const orders = [order1, order2, order3];
+
+    const listOfDeliveryDates = orders // all the orders [o1, o2, o3]
+      .filter(isUndeliveredOrder) // [o1, o2]
+      .map(calculateExpectedDeliveryDate); // [date, date]
+
+    console.log(listOfDeliveryDates);
   });
 });
--- syntax/src/shipping/index.ts
+++ syntax/src/shipping/index.ts
@@ -26,7 +26,7 @@ export type Order = PendingOrder | ShippedOrder | DeliveredOrder;
 // If it has been shipped, it should be expected from the shipDate + 3 days
 // If it has been already delivered, ???
 type UnDeliveredOrder = Exclude<Order, { status: "delivered" }>;
-export function calculateExpectedDeliveryDate(order: UnDeliveredOrder) {
+export function calculateExpectedDeliveryDate(order: UnDeliveredOrder): Date {
   switch (order.status) {
     case "shipped": {
       return new Date(); // figure it out
@@ -35,4 +35,8 @@ export function calculateExpectedDeliveryDate(order: UnDeliveredOrder) {
       return new Date();
     }
   }
+}
+
+export function isUndeliveredOrder(order: Order): order is UnDeliveredOrder {
+  return order.status !== "delivered";
 }
